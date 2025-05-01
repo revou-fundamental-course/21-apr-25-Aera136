fc0d1511f41ad6080bf6ab7efb4480d2446d56a6
Aera136 <aeroditamega13@gmail.com>
2025-05-01 22:48:24 +0700

    js(2)

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ function validateForm() {
 
     if (inputCelcius.value === '') {
         alert('Input tidak boleh kosong!');
-    } else {
-        /// Convert to Fahrenheit
     }
+    let celcius = parseFloat(document.getElementById('konversi-input').value);
+    if (isNaN(celcius)) {
+        alert('Masukkan angka yang valid!');
+        return false;
+    }
+    let fahrenheit = (celcius * 9/5) + 32;
+    document.getElementById('result-input').value = fahrenheit.toFixed(2);
+    document.getElementById('calculate-detail').value = `(${celcius} * 9/5) + 32 = ${fahrenheit.toFixed(2)}`;
 }
+
+function reverse() {
+    let fahrenheit = parseFloat(document.getElementById('result-input').value);
+    let celcius = (fahrenheit - 32) * 5/9;
+    document.getElementById('konversi-input').value = celcius.toFixed(2);
+    document.getElementById('calculate-detail').value = `(${fahrenheit} * 1.8) + 32 = ${celcius.toFixed(2)}`;
+}
+
+function reset() {
+    document.getElementById('konversi-input').value = '';
+    document.getElementById('result-input').value = '';
+    document.getElementById('calculate-detail').value = '';
+}
+
